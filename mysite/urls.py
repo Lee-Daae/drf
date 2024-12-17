2892df94a8f0466d759e13801472ff8016ee0ab9
Lee-Daae <da.lee@haezoom.com>
2024-12-17 12:23:55 +0900

    blog API

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,28 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
-# from django.urls import path, include
-
-# from mysite.views import HomeView
-
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     # shkim
-#     path('', HomeView.as_view(), name='home'),
-#     path('blog/', include('blog.urls')),
-#     path('api/', include('api.urls')),
-#     # drf
-#     path('api-auth/', include('rest_framework.urls')),
-# ]
-
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from mysite.views import HomeView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # shkim
+    path('', HomeView.as_view(), name='home'),
+    path('blog/', include('blog.urls')),
+    path('api/', include('api.urls')),
+    path('api2/', include('api2.urls')),
+    # drf
+    #path('api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# from django.contrib import admin
+# from django.urls import path, include
 # from django.contrib.auth.models import User
 # from rest_framework import routers, serializers, viewsets
 
@@ -55,9 +56,9 @@ from django.urls import path, include
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api2/', include('api2.urls')),
+#urlpatterns = [
+#   path('admin/', admin.site.urls),
+#    path('api2/', include('api2.urls')),
     # path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
+#]
