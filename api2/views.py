1679100a2558a0cda9997cb944177d72eadc2918
Lee-Daae <da.lee@haezoom.com>
2024-12-17 16:39:51 +0900

    PostList와 PostRetrieve의 Serializer 분리

--- api2/views.py
+++ api2/views.py
@@ -17,18 +17,17 @@
 #     queryset = Comment.objects.all()
 #     serializer_class = CommentSerializer
 
-from api2.serializers import CommentSerializer, PostSerializer
+from api2.serializers import CommentSerializer, PostListSerializer, PostRetrieveSerializer
 from blog.models import Comment, Post
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 
-
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostListSerializer
 
 class PostRetrieveAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostRetrieveSerializer
 
 class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
