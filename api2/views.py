fedf831bcc68413126b24fa200d938bc8a505fe7
Lee-Daae <da.lee@haezoom.com>
2024-12-19 18:23:26 +0900

    like api get 메소드로 바꿈

--- api2/views.py
+++ api2/views.py
@@ -17,9 +17,10 @@
 #     queryset = Comment.objects.all()
 #     serializer_class = CommentSerializer
 
-from api2.serializers import CateTagSerializer, CommentSerializer, PostLikeSerializer, PostListSerializer, PostRetrieveSerializer
+#from api2.serializers import CateTagSerializer, CommentSerializer, PostLikeSerializer, PostListSerializer, PostRetrieveSerializer
+from api2.serializers import CateTagSerializer, CommentSerializer, PostListSerializer, PostRetrieveSerializer
 from blog.models import Category, Comment, Post, Tag
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -35,27 +36,51 @@ class CommentCreateAPIView(CreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
-# patch 메소드 구현, UpdateAPIView는 put과 patch 만 가능
-class PostLikeAPIView(UpdateAPIView):
+# # patch 메소드 구현, UpdateAPIView는 put과 patch 만 가능
+# class PostLikeAPIView(UpdateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostLikeSerializer
+
+#     def update(self, request, *args, **kwargs):
+#         partial = kwargs.pop('partial', False)
+#         instance = self.get_object()
+#         # data = instance.like + 1 딕셔너리가 아니라 숫자만 나오게 하려고 하면 drf serializer : dict-like 기반이라 오류나서 response 바꿔야 됨
+#         data = {'like': instance.like + 1}
+#         serializer = self.get_serializer(instance, data=data, partial=partial)
+#         serializer.is_valid(raise_exception=True)
+#         self.perform_update(serializer)
+
+#         if getattr(instance, '_prefetched_objects_cache', None):
+#             # If 'prefetch_related' has been applied to a queryset, we need to
+#             # forcibly invalidate the prefetch cache on the instance.
+#             instance._prefetched_objects_cache = {}
+
+#         #return Response(serializer.data)
+#         return Response(data['like'])
+    
+# get 메소드 구현
+class PostLikeAPIView(GenericAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostLikeSerializer
+    #serializer_class = PostLikeSerializer
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
+    def get(self, request, *args, **kwargs):
         instance = self.get_object()
-        # data = instance.like + 1 딕셔너리가 아니라 숫자만 나오게 하려고 하면 drf serializer : dict-like 기반이라 오류나서 response 바꿔야 됨
-        data = {'like': instance.like + 1}
-        serializer = self.get_serializer(instance, data=data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        instance.like += 1 
+        instance.save()
+        # data = {'like': instance.like + 1}
+        # 역직렬화과정 주석처리
+        # serializer = self.get_serializer(instance, data=data, partial=partial)
+        # serializer.is_valid(raise_exception=True)
+        # self.perform_update(serializer)
 
-        if getattr(instance, '_prefetched_objects_cache', None):
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance.
-            instance._prefetched_objects_cache = {}
+        # if getattr(instance, '_prefetched_objects_cache', None):
+        #     # If 'prefetch_related' has been applied to a queryset, we need to
+        #     # forcibly invalidate the prefetch cache on the instance.
+        #     instance._prefetched_objects_cache = {}
 
         #return Response(serializer.data)
-        return Response(data['like'])
+        return Response(instance.like)
+
     
 class CateTagAPIView(APIView):
     # Cannot apply DjangoModelPermissionsOrAnonReadOnly on a view that does not set `.queryset` or have a `.get_queryset()` method.
