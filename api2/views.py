a4c7389feea5b62bb472e937965fa42656653b34
Lee-Daae <da.lee@haezoom.com>
2024-12-20 16:57:51 +0900

    router사용하지 않고 viewset으로 refactoring

--- api2/views.py
+++ api2/views.py
@@ -35,9 +35,9 @@ from rest_framework.viewsets import ModelViewSet
 #     queryset = Post.objects.all()
 #     serializer_class = PostRetrieveSerializer
 
-class CommentCreateAPIView(CreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+# class CommentCreateAPIView(CreateAPIView):
+#     queryset = Comment.objects.all()
+#     serializer_class = CommentSerializer
 
 # # patch 메소드 구현, UpdateAPIView는 put과 patch 만 가능
 # class PostLikeAPIView(UpdateAPIView):
@@ -114,35 +114,19 @@ class PostPageNumberPagination(PageNumberPagination):
             'pageCnt': self.page.paginator.num_pages,
             'curPage': self.page.number,
         })
-
-class PostListAPIView(ListAPIView):
+    
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
     pagination_class = PostPageNumberPagination
-# 'request':None 으로 링크 출력포맷 수정
+
     def get_serializer_context(self):
         return {
             'request':None,
             'format':self.format_kwarg,
             'view':self
         }
-
-def get_prev_next(instance):
-    try:
-        prev = instance.get_previous_by_update_dt()
-    except instance.DoesNotExist:
-        prev = None
     
-    try:
-        next_ = instance.get_next_by_update_dt()
-    except instance.DoesNotExist:
-        next_ = None
-
-    return prev, next_
-
-class PostRetrieveAPIView(RetrieveAPIView):
-    #queryset = Post.objects.all()
-    #serializer_class = PostSerializerDetail
     def get_queryset(self):
         return Post.objects.all().select_related('category').prefetch_related('tags', 'comment_set')
 
@@ -166,34 +150,13 @@ class PostRetrieveAPIView(RetrieveAPIView):
         }
 
         return Response(dataDict)
-
-        # data = {
-        #     'post' : instance,
-        #     'prevPost' : prevInstance,
-        #     'nextPost' : nextInstance,
-        #     'commentList' : commentList,            
-        # }
-        # serializer = self.get_serializer(instance=data)
-        # return Response(serializer.data)
-    # def get_serializer_context(self):
-    #     return {
-    #         'request':None,
-    #         'format':self.format_kwarg,
-    #         'view':self
-    #     }   postDict = obj_to_post(instance)
-        prevDict, nextDict = prev_next_post(instance)
-        commentDict = [obj_to_comment(c) for c in commentList]
-
-   
     
-class PostViewSet(ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostListSerializer
-    pagination_class = PostPageNumberPagination
-
-    def get_serializer_context(self):
-        return {
-            'request':None,
-            'format':self.format_kwarg,
-            'view':self
-        }
+    def like(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.like += 1 
+        instance.save()
+        return Response(instance.like)
+    
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
